92cdd69dae838ca1f928a92a4cb9dc05352851b0
iftekharx <iftekharx86@gmail.com>
2023-05-09 14:16:21 +0600

    buttons and themes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { Button, Typography } from '@mui/material'
+import { Add, Settings } from '@mui/icons-material'
+import styled from '@emotion/styled'
+
 function App() {
-  return <div className="App">Hello</div>
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: 'skyblue',
+    color: '#888',
+    margin: 5,
+    '&:hover': {
+      backgroundColor: 'lightblue',
+    },
+    '&:disabled': {
+      backgroundColor: 'gray',
+      color: 'white',
+    },
+  }))
+
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button startIcon={<Add />} variant="contained">
+        Add Button
+      </Button>
+
+      <Typography variant="h1" component="p">
+        It uses h1 style but it's a p tag
+      </Typography>
+      <BlueButton>Blue Button</BlueButton>
+      <BlueButton>Another Blue Button</BlueButton>
+    </div>
+  )
 }
 
 export default App
