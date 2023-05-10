17bddbe9872d45b3a6d49f80ba9b00bd99d48921
iftekharx <iftekharx86@gmail.com>
2023-05-10 09:27:52 +0600

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled'
+import { styled } from '@mui/material/styles'
 import { Notifications, Pets } from '@mui/icons-material'
 import { Mail } from '@mui/icons-material'
 import {
@@ -8,8 +8,11 @@ import {
   Box,
   InputBase,
   Badge,
+  Avatar,
+  Menu,
+  MenuItem,
 } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
@@ -19,13 +22,30 @@ const StyledToolbar = styled(Toolbar)({
 const Search = styled('div')(({ theme }) => ({
   backgroundColor: 'white',
   padding: '0 10px',
-  borderRadius: 10,
+  borderRadius: theme.shape.borderRadius,
   width: '40%',
 }))
 
-const Icons = styled(Box)(({ theme }) => ({}))
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  alignItems: 'center' /* center vertically */,
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}))
 
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center' /* center vertically */,
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}))
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -43,8 +63,40 @@ const Navbar = () => {
           <Badge badgeContent={2} color="error">
             <Notifications />
           </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj"
+            onClick={(e) => setOpen(true)}
+          />
         </Icons>
+
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj"
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
+
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
